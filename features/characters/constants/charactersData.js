0fe44a7a1c0554d70e72fbe726fe286195a30d87
Nilanjana Jui <nilanjana.csecu@gmail.com>
2026-03-24 23:51:04 +0600

    character data and card added

---
+++ features/characters/constants/charactersData.js
@@ -0,0 +1,85 @@
+// src/features/characters/constants/charactersData.js
+
+const CHARACTERS = [
+    {
+        id: 1,
+        name: "Harry Potter",
+        actor: "Daniel Radcliffe",
+        house: "Gryffindor",
+        status: "Alive",
+        image:
+            "https://lh3.googleusercontent.com/aida-public/AB6AXuB7mIX2mUrLAbwF1dmeFPnqtGlymtVoqfeIIBwCVHMWM5Q8jE0vzAhOM9kid_sEjbPciT91I2sfgsNXIFooqOsSs-8OzUoft5EzJjER_8Hlew2uvCx6XVygR-UOckunTjeX0LpR2tY6quLHBvrznEwHkMQUBswPSLj2KCLkOuIlSGTI8gG4RgOvIztrlPYG6fWHMzUvi1_N9qpDr3z2DYN8oOAj6cePTuaaKFybHUKB0vYl5HyPtOYabMTSSJ2nH1Q4SrMSJLLGdWAp",
+    },
+    {
+        id: 2,
+        name: "Hermione Granger",
+        actor: "Emma Watson",
+        house: "Gryffindor",
+        status: "Alive",
+        image:
+            "https://lh3.googleusercontent.com/aida-public/AB6AXuBenC2jTC5StdlhkmACASo3JGhFVNMiAC8pDg_wBPbATP-dH8TAjg4nBGaD3Wf_Nnp1r1ylIbAWZycf_bRMcFFED9JtgJO1Cqg5uSYDyw2GyPiRmFUT1PKFAFrvcwuuNfEjZRCneyl5PiJFPE8AnVDssRRu73q3jRkI5sWiai-ZsqcP3cIkPJPl-Z__Mv2WhetnB8Tr0tF2MIeMj67wozJ2hBIPwpf0QjqjLpznFmMFFNLsgWD8uzLjWfuSgZspdbIpYAdYV6lz29uH",
+    },
+    {
+        id: 3,
+        name: "Severus Snape",
+        actor: "Alan Rickman",
+        house: "Slytherin",
+        status: "Deceased",
+        image:
+            "https://lh3.googleusercontent.com/aida-public/AB6AXuAxYQsDFJ6WJTy-ifnrAsHx94xIjwNW4QjZ6crVZEmmmFk3hE3IqirnAQ9zwJMBC7faUIlBB3_n3X6V6ID8fqS0v08ueBfhN4Hom0MRxph8NEKzIlev99jyVgBQlpz3dng59u8XRZvT1uR0CWc_pmoLO876lgyH_7mhfin0JhFXyLwVXumG7xW7LX9Iu_4LMJ7QDSdVJOuIoPiPPq3mMfed2x0BuaaOYCiasWVUu3Ikusr4AShEpWNuue7v7rnBWZ8LPyZ_lclD_Wdc",
+    },
+    {
+        id: 4,
+        name: "Draco Malfoy",
+        actor: "Tom Felton",
+        house: "Slytherin",
+        status: "Alive",
+        image:
+            "https://lh3.googleusercontent.com/aida-public/AB6AXuD2-AwCRr3tDvR2lyWHn9ee8kYthszskWnpkRVKf5XY9qIinMz4mZCCz_kKCvv7Etw9C0elo7c8c2A-R7vs8OIPU0eMlVF-6gvgjuT1EquD32KX8-7ZOnB_3bMBaL_LGCQMWnBS1g8nqzCPftJU11eTkPxtEpXdAb-D2hLpgMoEDRGa-VGXZjtkXUfX9nc5L_d8wI_WLwyPGtCX8HHWRjUCdWRoAiGERhyaA64KWnCnUmOxrJu91FRMOhEiOPg1YbspbQ7j_GAZcpd5",
+    },
+    {
+        id: 5,
+        name: "Luna Lovegood",
+        actor: "Evanna Lynch",
+        house: "Ravenclaw",
+        status: "Alive",
+        image:
+            "https://lh3.googleusercontent.com/aida-public/AB6AXuC22-njFSIMveu1OCx82y9eI9l5xiUQ-IehBii5R8ZjOuUKY10-yQkKk2be565Uc0LSVqzHb1x9RJIAafSMa3AlnjHl_Eaz7h2N--O2eBMQL9zdWbpCMGH7BlkslRvo7TlGipnhnLkxVkGH_E4gI0U7FlhzZiwXGsPYw1SdPOhSNG-sySk6C_PTDkwE0gq6aTTFHEqtauznRnhO8Vqt1ofiZxRjy-EHP8H4KYFNLLua9Q4KPtCzgqMuAAmVYEtmA_wCPoGSpwYszTxh",
+    },
+    {
+        id: 6,
+        name: "Ron Weasley",
+        actor: "Rupert Grint",
+        house: "Gryffindor",
+        status: "Alive",
+        image:
+            "https://lh3.googleusercontent.com/aida-public/AB6AXuCIXCZbxm8aZrdC7gVcRvvyg4poEcdNxcGvVtATH0rjDwSTdY-wvi654kdNV8W4l1R32lSrD4ldGCThqh0WhzC6xXEe_ryZfINAmiRcn7egRRGsVpvyN_0CftQCCKk4pgH8VEMhPjsCBF4yuw00ousra8V0-zXLPQ2D0zhPDlXxJ-rcJDGCuVhzYb5wYFAeEDc1I_X2TeBikdifJAamsRjegH1gkFLgtn2BqFa94Z9yXXvkoZl56OE427cKG8K_ChG-dWVmbIs-kheW",
+    },
+    {
+        id: 7,
+        name: "Cedric Diggory",
+        actor: "Robert Pattinson",
+        house: "Hufflepuff",
+        status: "Deceased",
+        image:
+            "https://lh3.googleusercontent.com/aida-public/AB6AXuDQuo7Lehy4x5fch_hVSguuICYdSY8hfPRf2F90JQ1CSzCEVmB0WpcirOqPAoz9ZgiZPj21VWMmt6m3SWiEkU9u9bwvOWkNTgunWAIEbSZj3xDq1uBb3AdiTDhMO4jKCHOY4qOzjMof-HIGC2Irku4vV7psX1LVw5Mf_clMDyec-U7f5VLwHbmaT-iA8-Wnzn4yq1AUskVG1AFFb9Ct5Jb0KauDUcAX72VVrKb8B0p5TidSK4C4oYE_9TGjeZfv9FvgETf2lB2i7ozy",
+    },
+    {
+        id: 8,
+        name: "Albus Dumbledore",
+        actor: "Michael Gambon",
+        house: "Gryffindor",
+        status: "Deceased",
+        image:
+            "https://lh3.googleusercontent.com/aida-public/AB6AXuBk1_7BnoZN6yrgvqEqdK-SwxKbpOXJIgNn-Qk4nkORzuR-2ugGCVKPcluptEiLcIMywY1VhzqAOAvtD-UBEzlCSwd78Q2YyFvr-CFzMM24wqv57u29NyXluyoz7YnYUfQyV0nA6k0A6IERzLJdt-5-pT8yTAhfutaGWOQJsPcdBELOhXDxbKuVJi1h0RDF0MRX4Rcon82_avmhOs8DDorI3UTtijvaUnrXdRCR_QKnO9wzaoftnepfSnxtFdH1CKPR6FJyab6qU3ar",
+    },
+];
+
+export const HOUSE_COLORS = {
+    Gryffindor: "bg-primary/90 text-background-dark",
+    Slytherin: "bg-slate-700 text-white",
+    Ravenclaw: "bg-blue-800 text-white",
+    Hufflepuff: "bg-yellow-600 text-white",
+};
+
+export default CHARACTERS;
